9fdda121172ce18cf204034c7c51493532f2f1a1
petchill <songpon111333@gmail.com>
2025-10-04 15:37:34 +1000

    feat: init subsciber of following topic in notification subscriber

---
+++ internal/infrastructure/subscriber/notification.go
@@ -0,0 +1,62 @@
+package subscriber
+
+import (
+	"clone-instagram-service/internal/domain/model"
+	mRela "clone-instagram-service/internal/domain/model/relationship"
+	"context"
+	"encoding/json"
+	"log"
+	"time"
+
+	"github.com/segmentio/kafka-go"
+)
+
+type notificationSubscriber struct {
+	kafkaConfig model.KafkaConfig
+}
+
+func NewNotificationSubscriber(kafkaConfig model.KafkaConfig) *notificationSubscriber {
+	return &notificationSubscriber{
+		kafkaConfig: kafkaConfig,
+	}
+}
+
+func (sub *notificationSubscriber) SubscriberFollowing(callback func(ctx context.Context, message mRela.FollowingTopicMessage) error) error {
+	topic := "following"
+	r := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:        sub.kafkaConfig.Brokers,
+		Topic:          topic,
+		StartOffset:    kafka.FirstOffset, // Start at newest if no committed offset
+		CommitInterval: time.Second,       // auto-commit interval
+	})
+	defer r.Close()
+
+	go func() {
+		for {
+			m, err := r.ReadMessage(context.Background())
+			if err != nil {
+				continue
+			}
+
+			log.Printf("[partition=%d offset=%d] key=%s value=%s", m.Partition, m.Offset, string(m.Key), string(m.Value))
+			// Process the message
+			var message mRela.FollowingTopicMessage
+			err = json.Unmarshal(m.Value, &message) // Uncomment this line when the struct is filled
+			if err != nil {
+				log.Printf("Error unmarshaling message: %v", err)
+				continue
+			}
+			err = callback(context.Background(), message)
+			if err != nil {
+				log.Printf("Error processing message: %v", err)
+				continue
+			}
+			// Commit the message
+			if err := r.CommitMessages(context.Background(), m); err != nil {
+				continue
+			}
+		}
+	}()
+
+	return nil
+}
